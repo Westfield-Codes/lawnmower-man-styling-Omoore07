70dcedde5d56d8103c4b0f0a65de4fa61861c5c2
Omoore07 <Owen200036@student-schoolsofwestfield.org>
2025-06-11 14:03:11 +0000

    idk

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function setup(){
+let ok = document.createElement("button");
+ok.id="ok";
+button.appendChild("ok");
+ok.innerHTML = "ok";
+let document.getElementByID("ok");
+let cancel = document.createElement("button");
+
+
+
 // 7 lines of code, including (not in this order)
 // appendChild
 // createElement
